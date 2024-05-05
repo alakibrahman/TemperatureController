733f657c4fa8d4b138db3d3670fec92a0f48c2c7
Al Akib <alakibrahman@gmail.com>
2024-05-05 23:11:10 +0600

    rain can make temperature low

--- src/TemperatureControllerTest.java
+++ src/TemperatureControllerTest.java
@@ -15,17 +15,70 @@ TemperatureController passangerController1 , passangercontroller2 , passangerCon
     }
     @Test
     void getCurrentTemperatureForpassangerController1(){
+        passangerController1.up();
+        passangerController1.up();
+        passangerController1.up();
+        passangerController1.up();
+        passangerController1.down();
+        passangerController1.down();
+        passangerController1.up();
+        passangerController1.down();
+        assertEquals(102 , passangerController1.getCurrentTemperature());
+    }
 
-    }
     @Test
-    void up(){
+    void getCurrentTemperatureForpassangerController2(){
+        passangercontroller2.up();
+        passangercontroller2.up();
+        passangercontroller2.up();
+        passangercontroller2.up();
+        passangercontroller2.down();
+        passangercontroller2.down();
+        passangercontroller2.up();
+        passangercontroller2.down();
+        passangercontroller2.up();
+        passangercontroller2.down();
+        passangercontroller2.down();
+        passangercontroller2.up();
+        passangercontroller2.down();
+        assertEquals(32 , passangercontroller2.getCurrentTemperature());
     }
 
     @Test
-    void down(){
+    void getCurrentTemperatureForpassangerController3(){
+        passangerController3.up();
+        passangerController3.down();
+        passangerController3.up();
+        passangerController3.down();
+        passangerController3.down();
+        passangerController3.up();
+        passangerController3.down();
+        passangerController3.getCurrentTemperature();
     }
 
     @Test
-    void getCurrentTemperature(){
+    void getCurrentTemperatureForDriverController1(){
+        driverController1.up();
+        driverController1.up();
+        driverController1.up();
+        driverController1.up();
+        driverController1.down();
+        driverController1.down();
+        driverController1.up();
+        driverController1.down();
+        driverController1.getCurrentTemperature();
     }
+
+    @Test
+    void getCurrentTemperatureForDriverController2(){
+        driverController2.up();
+        driverController2.down();
+        driverController2.down();
+        driverController2.up();
+        driverController2.down();
+        driverController2.up();
+        driverController2,up();
+        driverController2.getCurrentTemperature();
+    }
+
 }
